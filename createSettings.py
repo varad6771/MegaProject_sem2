75fae6182f693ca79773d5afe1d25b5f9baeded6
Varad Vanjape <varadtech67@gmail.com>
2018-12-03 12:47:28 +0530

    made read and write functions more modular

--- createSettings.py
+++ createSettings.py
@@ -1,24 +1,26 @@
 import json
 from pathlib import Path
 
-
-def write_settings():
-    # TODO :- take input as args from other function. Args would be name, paswd, and 3 subproc args
-
-    input_name = input("Enter the name")
-    input_password = input("Enter the password")
-
-    print(input_name)
-    print(input_password)
+'''
+    This function writes the settings in settings.json
+    if the file is not there it will create the file and then store function else it will read settings
+    @params input_name, input_password, app1, app2, app3
+ '''
+ # TODO: to take the password in secure form instead of plaintext
+def write_settings(input_name,input_password,app1,app2,app3):
 
     data = {}
     data['Settings'] = []
     data['Settings'].append({
         'name': input_name,
         'password': input_password,
+        'app1': app1,
+        'app2': app2,
+        'app3': app3,
     })
 
-    ''' 
+    '''
+        TODO:
         first check whether folder is there then check whether file is there
         check the path depending upon where a folder will be created
         folder will be used to store some of the important settings and other tidbits
@@ -26,24 +28,32 @@ def write_settings():
 
     settingsFile = Path("settings.json")
     if settingsFile.is_file():
-        print("File is present")
-        read_settings()
+        print("File is present") # do some processing
     else:
         with open('settings.json', 'w') as outfile:
-            json.dump(data, outfile)
+            json.dump(data, outfile) # settings written to file
 
 
+'''
+    This function reads the data from file.
+    @return array of settings :- input_data[]
+'''
 def read_settings():
-    print("in read settings")
+
     with open('settings.json') as json_file:
         input_data = json.load(json_file)
-    for p in input_data['Settings']:
-        print('Name: ' + p['name'])
-        print('Password: ' + p['password'])
-        print('')
-    # TODO :- pass read subprocess args to another function
+
+    return input_data
 
 
-if __name__ == '__main__':
-    write_settings()
-    read_settings()
+# if __name__ == '__main__':
+#     write_settings('varad', 'vanjape', '$HOME/Code/MegaProject_sem2/app1','$HOME/Code/MegaProject_sem2/app2','$HOME/Code/MegaProject_sem2/app3')
+#     read_settings()
+# example of how to access the data from file
+# for settings_data_file in input_data['Settings']:
+#     print('Name: ' + settings_data_file['name'])
+#     print('Password: ' + settings_data_file['password'])
+#     print('app1: ' + settings_data_file['app1'])
+#     print('app2: ' + settings_data_file['app2'])
+#     print('app3: ' + settings_data_file['app3'])
+#     print('')
