0f36ae495551171908fa07c6c0b12fdbcd2f2af1
Varad Vanjape <varadtech67@gmail.com>
2019-02-18 23:58:10 +0530

    back end connected. need to align cropping window to match correctly
    Also replace code of master for AppUI.py, it currently of Development so it would not work

---
+++ utils/detector_utils.py
@@ -0,0 +1,101 @@
+# Utilities for object detector.
+
+import numpy as np
+import sys
+import tensorflow as tf
+import os
+from threading import Thread
+from datetime import datetime
+import cv2
+from utils import label_map_util
+from collections import defaultdict
+
+detection_graph = tf.Graph()
+
+TRAINED_MODEL_DIR = 'frozen_graphs'
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = TRAINED_MODEL_DIR + '/ssd5_optimized_inference_graph.pb'
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = TRAINED_MODEL_DIR + '/hand_label_map.pbtxt'
+
+NUM_CLASSES = 1
+# load label map using utils provided by tensorflow object detection api
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(
+    label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+# Load a frozen infrerence graph into memory
+def load_inference_graph():
+
+    # load frozen tensorflow model into memory
+    print("> ====== Loading frozen graph into memory")
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        sess = tf.Session(graph=detection_graph)
+    print(">  ====== Inference graph loaded.")
+    return detection_graph, sess
+
+# Drawing bounding boxes and distances onto image
+def draw_box_on_image(num_hands_detect, score_thresh, scores, boxes, classes, im_width, im_height, image_np):
+    # Determined using a piece of paper of known length, code can be found in distance to camera
+   # focalLength = 875
+    # The average width of a human hand (inches) http://www.theaveragebody.com/average_hand_size.php
+    # added an inch since thumb is not included
+   # avg_width = 4.0
+    # To more easily differetiate distances and detected bboxes
+    global p1
+    global p2
+    color = (0,255,0)
+    for i in range(num_hands_detect):
+        if (scores[i] > score_thresh):
+            (left, right, top, bottom) = (boxes[i][1] * im_width, boxes[i][3] * im_width,
+                                          boxes[i][0] * im_height, boxes[i][2] * im_height)
+            p1 = (int(left), int(top))
+            p2 = (int(right), int(bottom))
+
+            cv2.rectangle(image_np, p1, p2, color , 3, 1)
+            
+            # need to cheange dimensions according to image check it in images folder and then cheange
+            # accordingly
+            roi = image_np[int(left):int(right), int(top):int(bottom)]
+            file = "/home/varadvanjape/Code/Python/Sem_2/MegaProject_sem2/Images/image1.png"
+            cv2.imwrite(file,roi)
+            #print("image cropped")
+            
+            cv2.putText(image_np, 'hand', (int(left), int(top)-5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5 , color, 2)
+
+            cv2.putText(image_np, 'Accuracy: '+str("{0:.2f}".format(scores[i])),
+                        (int(left),int(top)-20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+
+# Actual detection .. generate scores and bounding boxes given an image
+def detect_objects(image_np, detection_graph, sess):
+    # Definite input and output Tensors for detection_graph
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+    detection_boxes = detection_graph.get_tensor_by_name(
+        'detection_boxes:0')
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name(
+        'detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name(
+        'detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name(
+        'num_detections:0')
+
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores,
+            detection_classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+    return np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes)
